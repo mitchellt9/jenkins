9c9edb48f66a7debe2571a06a5f1bcf9b52c3389
mitchellt9 <mitchell.t9@gmail.com>
2022-04-18 20:10:30 +0530

    plural changes

--- src/MitchellGit.java
+++ src/MitchellGit.java
@@ -3,6 +3,6 @@ public class MitchellGit {
         // write your code here
         for (int i=1;i<=5;i++)
         {
-            System.out.println("Hello Git, I am your commit to check Jenkins Build jobs");
+            System.out.println("Hello Gitians, I am your commit to check Jenkins Build jobs");
         }
 }}
