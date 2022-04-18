f5012063b0aae0f83b4cf3e6e9f432070c0310bb
mitchellt9 <mitchell.t9@gmail.com>
2022-04-18 16:12:25 +0530

    my first commit for this folder

---
+++ src/MitchellGit.java
@@ -0,0 +1,8 @@
+public class MitchellGit {
+    public static void main(String[] args) {
+        // write your code here
+        for (int i=1;i<=5;i++)
+        {
+            System.out.println("Hellow");
+        }
+}}
